531c06e72528f43ae1766fa90affa6b73d6961e1
Vikas Prajapat <vikasprajapat@Vikass-MacBook-Air.local>
2026-02-17 14:07:44 +0530

    auth

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use('/gurukripa', user);
 
 const PORT = process.env.PORT | 8000;
 
+app.get('/',(req,res)=>{
+  return res.status(200).json({
+    message:"Welcome To Guru Kripa"
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Server is Running on PORT = ${PORT}`);
 });
